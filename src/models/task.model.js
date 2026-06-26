79344175191e16a0f11bb07e7c2ac2d062d4de49
Rahul Singh <rahul.singh01711@gmail.com>
2026-06-26 13:51:58 +0530

    Add task model

---
+++ src/models/task.model.js
@@ -0,0 +1,52 @@
+
+const mongoose = require('mongoose')
+
+const taskSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: [true, "Title is required"],
+        trim: true,
+        maxlength: [100, 'Title cannot exceed 100 characters.'],
+    },
+
+    description:{
+        type: String,
+        trim: true,
+        maxlength: [500 , 'Description cannot exceed 500 characters'],
+    },
+
+    status:{
+        type: String,
+        enum: ['todo', 'pending', 'completed'],
+        default: 'todo',
+    },
+
+    priority: {
+        type: String,
+        enum: ['low', 'medium', 'high'],
+        default: 'midum',
+    },
+
+    dueDate:{
+        type: Date,
+    },
+
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        default: null,
+    },
+},
+{
+    timestamps: true,
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
